ce9223cdd099efd939cf2e755059aa882ca65b0d
Inosan <inosan2503@gmail.com>
2019-03-27 23:26:59 +0900

    retry

--- Documents/GitHub/SeikoFesta60th/SeikoFesta60th/ExhibitionViewController.swift
+++ Documents/GitHub/SeikoFesta60th/SeikoFesta60th/ExhibitionViewController.swift
@@ -15,12 +15,11 @@ class ExhibitionViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-       
+        // Dispose of any resources that can be recreated.
     }
-    
-
     /*
     // MARK: - Navigation
 
