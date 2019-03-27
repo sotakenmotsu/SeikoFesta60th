1fe1b7d7ee6730be564f81ff708193c0c6a2e5df
Inosan <inosan2503@gmail.com>
2019-03-28 00:19:17 +0900

    retry3

---
+++ SeikoFesta60th/SeikoFesta60th/ExhibitionViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ExhibitionViewController.swift
+//  SeikoFesta60th
+//
+//  Created by 剱物蒼太 on 2019/03/22.
+//  Copyright © 2019年 剱物蒼太. All rights reserved.
+//
+
+import UIKit
+
+class ExhibitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
