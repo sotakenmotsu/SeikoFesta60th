53198e7e7b624332d93dc44690891d01ada191b8
剱物蒼太 <sotakenmotsu@gmail.com>
2019-03-31 00:10:05 +0900

    QRコード読み取り成功

--- SeikoFesta60th/TreasureHuntViewController.swift
+++ SeikoFesta60th/TreasureHuntViewController.swift
@@ -7,16 +7,65 @@
 //
 
 import UIKit
+import AVFoundation
+import Foundation
 
-class TreasureHuntViewController: UIViewController {
+class TreasureHuntViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    private let session = AVCaptureSession()
+    let userDefaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let discoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: .back)
+        let devices = discoverySession.devices
+        
+        if let backCamera = devices.first {
+            do {
+                let deviceInput = try AVCaptureDeviceInput(device: backCamera)
+                if self.session.canAddInput(deviceInput) {
+                    self.session.addInput(deviceInput)
+                    let metadataOutput = AVCaptureMetadataOutput()
+                    if self.session.canAddOutput(metadataOutput) {
+                        self.session.addOutput(metadataOutput)
+                        metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+                        metadataOutput.metadataObjectTypes = [.qr]
+                        let previewLayer = AVCaptureVideoPreviewLayer(session: self.session)
+                        previewLayer.frame = self.view.bounds
+                        previewLayer.videoGravity = .resizeAspectFill
+                        self.view.layer.addSublayer(previewLayer)
+                        self.session.startRunning()
+                    }
+                }
+            } catch {
+                print("Error occured while creating video device input: \(error)")
+            }
+        }
 
         // Do any additional setup after loading the view.
     }
     
-    //githubどうなってる
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        for metadata in metadataObjects as! [AVMetadataMachineReadableCodeObject] {
+            if metadata.type != .qr { continue }
+            if metadata.stringValue == nil { continue }
+            if metadata.stringValue == "テスト" {
+                self.session.stopRunning()
+                self.showAlert()
+            }
+        }
+    }
+    
+    func showAlert() {
+        let alert = UIAlertController(title: "読み取り成功！", message:"カメラ画面へ戻ります", preferredStyle: UIAlertController.Style.alert)
+        let ok = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action: UIAlertAction) in
+            print("alertaction")
+            self.session.startRunning()
+        } )
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
     
 
    
